b6958c97efd0f251b47d8b50731c48b6f8d81765
Михаил Ренжин <renzhin@gmail.com>
2024-01-17 12:59:40 +0300

    Добавить перечень пустых классов моделей в models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Recipe(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class RecipeTag(models.Model):
+    pass
+
+
+class Ingredient(models.Model):
+    pass
+
+
+class RecipeIngredient(models.Model):
+    pass
+
+
+class Measurement(models.Model):
+    pass
+
+
+class Favourite(models.Model):
+    pass
+
+
+class Shoplist(models.Model):
+    pass
