4da418fbfa376a7cd248c659df8f002bf3fcd8ec
Михаил Ренжин <renzhin@gmail.com>
2024-01-18 15:03:51 +0300

    Запустить админ-зону: добавить модели, перевод

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Follow, Recipe, Ingredient, Measurement, Tag, Favourite, Shoplist
+)
+
+admin.site.register(Follow)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Measurement)
+admin.site.register(Tag)
+admin.site.register(Favourite)
+admin.site.register(Shoplist)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -19,12 +19,14 @@ class Follow(models.Model):
         blank=True,
         null=True,
         related_name='following',
+        verbose_name='подписчик',
     )
     following = models.ForeignKey(
         User, on_delete=models.CASCADE,
         blank=True,
         null=True,
         related_name='followers',
+        verbose_name='на кого подписан',
     )
 
     def __str__(self):
@@ -32,11 +34,18 @@ class Follow(models.Model):
 
     class Meta:
         unique_together = ('follower', 'following')
+        verbose_name = 'подписка'
+        verbose_name_plural = 'Подписки'
 
 
 class Tag(BaseModel):
-    name = models.CharField(max_length=128)
-    color = models.IntegerField()  # уточнить
+    name = models.CharField(
+        max_length=128,
+        verbose_name='название',
+    )
+    color = models.IntegerField(
+        verbose_name='цвет',
+    )  # уточнить
     slug = models.SlugField(max_length=50, unique=True)
     created_at = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
@@ -45,16 +54,30 @@ class Tag(BaseModel):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'Теги'
+
 
 class Measurement(BaseModel):
-    type = models.CharField(max_length=128)
+    type = models.CharField(
+        max_length=128,
+        verbose_name='тип',
+    )
 
     def __str__(self):
         return self.type
 
+    class Meta:
+        verbose_name = 'единица измерения'
+        verbose_name_plural = 'Единицы измерения'
+
 
 class Ingredient(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(
+        max_length=256,
+        verbose_name='название',
+    )
     measurement_unit = models.ForeignKey(
         Measurement, on_delete=models.CASCADE
     )
@@ -62,25 +85,37 @@ class Ingredient(BaseModel):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
 
 class Recipe(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(
+        max_length=256,
+        verbose_name='название',
+    )
     tag = models.ManyToManyField(Tag, through='TagRecipe')
     ingredient = models.ManyToManyField(Ingredient, through='IngredientRecipe')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='recipes'
     )
-    text = models.TextField()
+    text = models.TextField(verbose_name='описание',)
     image = models.ImageField(
         upload_to='recipes/', null=True, blank=True
     )  # разобраться
-    cooking_time = models.CharField(max_length=50)  # разобраться с типом поля
+    cooking_time = models.CharField(
+        max_length=50,
+        verbose_name='время приготовления',
+    )  # разобраться с типом поля
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['-created_at']
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'Рецепты'
 
 
 class TagRecipe(models.Model):
@@ -111,6 +146,8 @@ class Favourite(models.Model):
 
     class Meta:
         unique_together = ('user', 'recipe')
+        verbose_name = 'изранное'
+        verbose_name_plural = 'Избранное'
 
 
 class Shoplist(models.Model):
@@ -126,3 +163,5 @@ class Shoplist(models.Model):
 
     class Meta:
         unique_together = ('user', 'recipe')
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Список покупок'
