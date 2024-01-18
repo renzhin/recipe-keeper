b6cc99b61c1ba1a02bfd9618bc01535e06881ee9
Михаил Ренжин <renzhin@gmail.com>
2024-01-18 23:16:58 +0300

    Добавить поля в админку из связей ManyToMany + первично установить rest_framework и djoser

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
-
 from .models import (
-    Follow, Recipe, Ingredient, Measurement, Tag, Favourite, Shoplist
+    Follow,
+    Recipe,
+    Ingredient,
+    Measurement,
+    Tag,
+    TagRecipe,
+    IngredientRecipe,
+    Favourite,
+    Shoplist,
 )
 
+
+class TagRecipeInline(admin.TabularInline):
+    model = TagRecipe
+    extra = 1
+
+
+class IngredientRecipeInline(admin.TabularInline):
+    model = IngredientRecipe
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [TagRecipeInline, IngredientRecipeInline]
+
+
 admin.site.register(Follow)
-admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(Measurement)
 admin.site.register(Tag)
