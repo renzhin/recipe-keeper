9b8270bebd5b3b4e8c35091016bbd05527dcc522
Михаил Ренжин <renzhin@gmail.com>
2024-02-02 17:15:10 +0300

    Правки настроек nginx + настройки переменных в settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--z&iivk^dptz!8lqioj=kso_0#q_a%826ts)mgo)ram==k9si&'
+SECRET_KEY = os.getenv('SECRET_KEY', "Секретный ключ отсутсвует")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE', "Значения режима отладки отсутсвует")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    'ALLOW_HOSTS', "Значение списка разрешенных хостов отсутсвует"
+).split()
 
 
 # Application definition
@@ -142,8 +144,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
