6d9fd767835654d075b6d89b28348a969332307c
Михаил Ренжин <renzhin@gmail.com>
2024-02-13 09:23:08 +0300

    Добавить проверуку в импорте на наличие ингредиента в базе

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -17,7 +17,20 @@ class Command(BaseCommand):
             f'{data_path}/ingredients.json', 'r', encoding='utf-8'
         ) as file:
             data = json.load(file)
+
             for item in data:
+                # Проверяем, существует ли ингредиент с таким именем в базе
+                existing_ingredient = Ingredient.objects.filter(
+                    name=item['name']
+                ).first()
+                if existing_ingredient:
+                    # Если ингредиент с таким именем уже существует,
+                    # выводим сообщение и пропускаем его
+                    self.stdout.write(self.style.WARNING(
+                        f'Ингредиент "{item["name"]}" уже в базе. Пропускаем.'
+                    ))
+                    continue
+
                 # Получаем или создаем экземпляр модели Measurement
                 measurement_inst, created = Measurement.objects.get_or_create(
                     t_name=item['measurement_unit']
